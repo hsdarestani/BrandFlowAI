2e46e36a3e08f2cdc8d50782992f9f8819a26a7e
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-08-10 01:17:28 +0330

    Remove route layout that discarded Integrations children

--- apps/web/app/[locale]/app/integrations/layout.tsx
+++
@@ -1,6 +0,0 @@
-import {FunctionalIntegrationsPage} from '@/components/functional-integrations-page';
-
-export default async function Layout({params}:{params:Promise<{locale:string}>;children:React.ReactNode}){
- const {locale}=await params;
- return <FunctionalIntegrationsPage locale={locale}/>;
-}
